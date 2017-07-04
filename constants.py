3cecc9003067f1aaad845ba2bd46b135de49c703
Jyoti <jyotithakur15111@gmail.com>
2017-07-04 20:22:11 +0530

    user id

---
+++ constants.py
@@ -0,0 +1,5 @@
+#All GLOBAL VARIABLE HERE
+#intagram api access token
+APP_ACCESS_TOKEN='4190470390.905baef.deede7ee1396415d96b4b905018b8e33'
+#intagram api base url
+BASE_URL='https://www.google.co.in'
