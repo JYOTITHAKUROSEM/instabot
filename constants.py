c819b357d47c9a6b0ecd58736466ddef784f265a
Jyoti <jyotithakur15111@gmail.com>
2017-07-05 14:14:54 +0530

    constants file

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@
 #intagram api access token
 APP_ACCESS_TOKEN='4190470390.905baef.deede7ee1396415d96b4b905018b8e33'
 #intagram api base url
-BASE_URL='https://www.google.co.in'
+BASE_URL='https://api.instagram.com/v1/'
