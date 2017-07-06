b651595e3363d8c30b81692e8c76fe33b3e06f30
Jyoti <jyotithakur15111@gmail.com>
2017-07-06 12:44:57 +0530

    self info

--- self_info.py
+++ self_info.py
@@ -7,15 +7,15 @@ def self_info():
     user_info=requests.get(request_url).json()
     if user_info['meta']['code']==200:
         if len(user_info['data']):
-            print 'User Name:%s'%(user_info['data']['User Name'])
-            print 'No.Of Followers:%s'%(user_info['data']['counts']['followed By'])
-            print 'No.Of People You Are Following:%s'%(user_info['data']['counts']['follows'])
-            print 'No. Of Posts:%s' %(user_info['data']['counts']['media'])
+            print 'User Name:%s'%(user_info['data']['username'])
+            print 'No.of followers:%s'%(user_info['data']['counts']['followed_by'])
+            print 'No.of people You Are Following:%s'%(user_info['data']['counts']['follows'])
+            print 'No. Of posts:%s' %(user_info['data']['counts']['media'])
         else:
             print 'User Doesnot Exist!'
     else:
         print 'Status Code Other Then 200 Received'
-
+self_info()
 
 
 
