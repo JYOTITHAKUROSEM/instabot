f28b95d8301024fdaeefe944961b5467e20c8434
Jyoti <jyotithakur15111@gmail.com>
2017-07-06 12:44:04 +0530

    getting user id

--- get_user_id.py
+++ get_user_id.py
@@ -1,15 +1,18 @@
 import requests
 from constants import APP_ACCESS_TOKEN,BASE_URL
+insta_username = "sharmatanu9878"
 def get_user_id(insta_username):
     #function logic
     request_url=(BASE_URL +'users/search?q=%s&access_token=%s')%(insta_username,APP_ACCESS_TOKEN)
-    print 'GET REQUEST url:%'%(request_url)
+    print 'GET REQUEST url:%s'%(request_url)
     user_info=requests.get(request_url).json()
     if user_info['meta']['code']==200:
         if len(user_info['data']):
-            return user_info['data'][0]['id']
+           print((user_info['data'][0]['id']))
+           return(user_info['data'][0]['id'])
         else:
             return None
     else:
         print 'status code other then 200 received'
         exit()
+get_user_id(insta_username)
