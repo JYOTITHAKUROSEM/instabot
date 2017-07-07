05a0bf368efc2f184a0dd163c69e7273d006641d
Jyoti <jyotithakur15111@gmail.com>
2017-07-07 16:50:37 +0530

    all files commited

--- delete_negative_comment.py
+++ delete_negative_comment.py
@@ -1,29 +1,30 @@
 #Import Files
-import requests
+import requests,urllib
+from constants import  BASE_URL
 from textblob import TextBlob
-from constants import BASE_URL,APP_ACCESS_TOKEN
-from get_post_id import get_post_id
 from textblob.sentiments import NaiveBayesAnalyzer
-#from post_a_comment import post_a_comment
+from get_post_id import get_post_id
+APP_ACCESS_TOKEN="5684396832.5c38a37.96ddf1cebba74e5f94dd6135fad5392c"
+from get_user_id import get_user_id
+#insta_username = "sharmtanu"
 def delete_negative_comment(insta_username):
-    #Function Logic Here
-    media_id=get_post_id(insta_username)
-    request_url = (BASE_URL + 'media/%s/comments/?access_token=%s')%(media_id,APP_ACCESS_TOKEN)
+    media_id = get_post_id(insta_username)
+    request_url = (BASE_URL + 'media/%s/comments/?access_token=%s') % (media_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' %(request_url)
     comment_info = requests.get(request_url).json()
-    if comment_info['meta']['code'] ==200:
+    if comment_info['meta']['code'] == 200:
         if len(comment_info['data']):
            #Here's a naive implementation of how to delete the negative comments
-           for x in range(0,len(comment_info['data'])):
+           for x in range(0, len(comment_info['data'])):
                comment_id = comment_info['data'][x]['id']
                comment_text = comment_info['data'][x]['text']
                blob = TextBlob(comment_text, analyzer=NaiveBayesAnalyzer())
                if (blob.sentiment.p_neg > blob.sentiment.p_pos):
                    print 'Negative comment : %s' % (comment_text)
-                   delete_url = (BASE_URL + 'media/%s/comments/%s/?access_token=%s')% (media_id,comment_id,APP_ACCESS_TOKEN)
+                   delete_url = (BASE_URL + 'media/%s/comments/%s/?access_token=%s') % (media_id, comment_id, APP_ACCESS_TOKEN)
                    print 'DELETE request url : %s' % (delete_url)
                    delete_info = requests.delete(delete_url).json()
-                   if delete_info['meta']['code']==200:
+                   if delete_info['meta']['code'] == 200:
                        print 'Comment Successfully deleted!\n'
                    else:
                        print 'SORRY! Unable To Delete comment!'
@@ -32,5 +33,5 @@ def delete_negative_comment(insta_username):
         else:
              print 'Already Have comments on the post!'
     else:
-         print("Status code other than 200 received!")
+         print 'Status code other than 200 received!'
 delete_negative_comment(insta_username="sharmatanu9878")
